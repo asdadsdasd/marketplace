0fadc8aa5eaec377f17867c489278fd5e5cd6a8a
asdadsdasd <mkozarez12345@mail.ru>
2026-05-24 23:53:17 +0300

    Added @Repository annotation. Changed dto style in user service

--- user-service/src/main/java/com/example/userservice/dto/RegisterRequest.java
+++ user-service/src/main/java/com/example/userservice/dto/RegisterRequest.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
-    @Email String email,
-    @Size(min = 5) String password
+    @Email
+    String email,
+
+    @Size(min = 5)
+    String password
 ) {}
